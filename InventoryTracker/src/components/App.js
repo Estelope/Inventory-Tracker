d029a660b34311cfda3d0fd9105b0080ffbb9f36
Estelope <elopez14567@gmail.com>
2024-01-13 00:30:10 -0700

    add modified boilerplate

---
+++ InventoryTracker/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import InventoryControl from "./InventoryControl";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <InventoryControl />
+    </>
+  );
+}
+
+export default App;
