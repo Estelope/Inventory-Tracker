42ce0c0786cbe8335b5fa37a9a7b21dd629113c1
Estelope <elopez14567@gmail.com>
2024-01-16 00:11:36 -0700

    logic for amount details

--- InventoryTracker/src/components/ItemDetail.js
+++ InventoryTracker/src/components/ItemDetail.js
@@ -2,16 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ItemDetail(props){
-  const { item, onClickingDelete, onClickingEdit } = props; 
+  const { item, onClickingDelete, onClickingEdit, onClickingUpdateAmount } = props; 
+
+  function handleDecrementingAmount(){
+    onClickingUpdateAmount(item, -1);
+  }
+  
+  function handleIncrementingAmount(){
+    onClickingUpdateAmount(item, 1);
+  }
 
   return (
     <React.Fragment>
-      <h1>Bean Details</h1>
+      <h1>This Bean's Details</h1>
       <h3>{item.origin} - {item.names} - {item.roast}</h3>
       <p><em>{item.amount}</em></p>
-      <button onClick={onClickingEdit}>Update Item</button>
-      <button onClick={()=> onClickingDelete(item.id)}>Delete item </button>
-      <hr/>
+      <button onClick={handleIncrementingAmount}>Increase Amount</button>
+      <button onClick={handleDecrementingAmount}>Decrease Amount</button>
+      <br />
+      <button onClick={() => onClickingEdit(item.id)}>Edit </button>
+      <button onClick={() => onClickingDelete(item.id)}>Delete</button>
+      <br />
     </React.Fragment>
   );
 }
