050f0ae1ff2b9c08499e272fdb5a1caf5d2333c5
Estelope <elopez14567@gmail.com>
2024-01-16 19:19:58 -0700

    fix routing for onClickingEdit, fix indention

--- InventoryTracker/src/components/InventoryControl.js
+++ InventoryTracker/src/components/InventoryControl.js
@@ -67,14 +67,14 @@ class InventoryControl extends React.Component {
 
   handleUpdateItemAmount = (itemToUpdate, delta) => {
     const updatedAmount = itemToUpdate.amount + delta;
-    
-    if (updatedAmount >= 1 ) {
+
+    if (updatedAmount > 0) {
       const updatedItem = { ...itemToUpdate, amount: updatedAmount };
-      
+
       const newMainInventoryList = this.state.mainInventoryList
         .filter(item => item.id !== itemToUpdate.id)
         .concat(updatedItem);
-  
+
       this.setState({
         mainInventoryList: newMainInventoryList,
         selectedItem: updatedItem
@@ -86,20 +86,20 @@ class InventoryControl extends React.Component {
     let currentlyVisibleState = null;
     let buttonText = null;
     if (this.state.editing) {
-      currentlyVisibleState = <EditItemForm item={this.state.selectedItem} onEditItem={this.handleEditingItemInList} />
+      currentlyVisibleState = < EditItemForm item={this.state.selectedItem} onEditItem={this.handleEditingItemInList} />
       buttonText = "Return to Inventory List";
     } else if (this.state.selectedItem != null) {
       currentlyVisibleState = <ItemDetail
         item={this.state.selectedItem}
         onClickingDelete={this.handleDeletingItem}
-        onClickingEdit={this.handleEditingItemInList}
-        onClickingUpdateAmount={this.handleUpdateItemAmount} /> 
+        onClickingEdit={this.handleEditClick}
+        onClickingUpdateAmount={this.handleUpdateItemAmount} />
       buttonText = "Return to Inventory List";
     } else if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewItemForm onNewItemCreation={this.handleAddingNewItemToList} />;
       buttonText = "Return to Inventory List";
     } else {
-      currentlyVisibleState = <InventoryList onItemSelection={this.handleChangingSelectedItem} inventoryList={this.state.mainInventoryList} />;
+      currentlyVisibleState = < InventoryList onItemSelection={this.handleChangingSelectedItem} inventoryList={this.state.mainInventoryList} />;
       buttonText = "Add Item";
     }
     return (
